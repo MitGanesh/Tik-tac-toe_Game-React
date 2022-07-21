6a84c26be8ffc1b3df6e915a6cadee8338182abb
MitGanesh <ganeshmittha88@gmail.com>
2022-07-21 14:14:03 +0530

    Fixed Match Draw Issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ const array = new Array(9).fill("null")
 function App() {
   const [isCross, setisCross] = useState(false);
   const [winMessage, setwinMessage] = useState("");
+  const [flag, setflag] = useState(false)
 
+  let nullCount = 9;
+  
   function changeIcon(index) {
     // console.log("Cell Cliked");
     if (winMessage) {
@@ -20,33 +23,48 @@ function App() {
       alert("Already Filled !")
     }
     checkWinner()
+    checkForDraw()
+    console.log(flag);
   }
 
   function checkWinner() {
-    if (array[0]===array[1] && array[0]===array[2] && array[0]!== "null" ) {
+    if (array[0] === array[1] && array[0] === array[2] && array[0] !== "null") {
       setwinMessage(`${array[0]} Won the Game`);
-    } else if (array[3]===array[4] && array[3]===array[5] && array[3]!== "null") {
+    } else if (array[3] === array[4] && array[3] === array[5] && array[3] !== "null") {
       setwinMessage(`${array[3]} Won the Game`);
-    }else if (array[6]===array[7] && array[6]===array[8] && array[6]!== "null") {
+    } else if (array[6] === array[7] && array[6] === array[8] && array[6] !== "null") {
       setwinMessage(`${array[6]} Won the Game`);
-    }else if (array[0]===array[3] && array[0]===array[6] && array[0]!== "null") {
+    } else if (array[0] === array[3] && array[0] === array[6] && array[0] !== "null") {
       setwinMessage(`${array[0]} Won the Game`);
-    }else if (array[1]===array[4] && array[1]===array[7] && array[1]!== "null") {
+    } else if (array[1] === array[4] && array[1] === array[7] && array[1] !== "null") {
       setwinMessage(`${array[1]} Won the Game`);
-    }else if (array[2]===array[5] && array[2]===array[8] && array[2]!== "null") {
+    } else if (array[2] === array[5] && array[2] === array[8] && array[2] !== "null") {
       setwinMessage(`${array[2]} Won the Game`);
-    }else if (array[0]===array[4] && array[0]===array[8] && array[0]!== "null") {
+    } else if (array[0] === array[4] && array[0] === array[8] && array[0] !== "null") {
       setwinMessage(`${array[0]} Won the Game`);
-    }else if (array[2]===array[4] && array[2]===array[6] && array[2]!== "null") {
+    } else if (array[2] === array[4] && array[2] === array[6] && array[2] !== "null") {
       setwinMessage(`${array[2]} Won the Game`);
     }
   }
 
-  function reload () {
+  function reload() {
     // console.log("Reload Game");
     setisCross(false);
     setwinMessage("");
-    array.fill('null',0,9);
+    array.fill('null', 0, 9);
+    setflag(false);
+  }
+
+  function checkForDraw() {
+    array.forEach(ele => {
+      if (ele !== 'null') {
+        nullCount--;
+        // console.log(nullCount);
+        if (nullCount === 0) {
+          setflag(true);
+        }
+      }
+    });
   }
 
 
@@ -60,7 +78,14 @@ function App() {
         </div>
       ) : (
         <div className='my-5'>
-          <h1 className='text-uppercase'>Draw {isCross ? "Cross" : "Circle"}</h1>
+          {
+            flag ? (
+              <h1 className='text-uppercase'>Match Draw</h1>
+            ):(
+              <h1 className='text-uppercase'>Draw {isCross ? "Cross" : "Circle"}</h1>
+            )
+          }
+          {flag && <button type="button" className="btn btn-info" onClick={reload}>Reset</button>}
         </div>
       )}
 
