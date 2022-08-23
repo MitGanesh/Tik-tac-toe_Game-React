4591934409633f670ad279fff52a97670885cd04
MitGanesh <ganeshmittha88@gmail.com>
2022-08-23 18:23:10 +0530

    Landing page added

---
+++ src/Components/Home.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import '../App.css';
+import Icon from './Icon';
+
+const array = new Array(9).fill("null")
+
+function Home() {
+  const [isCross, setisCross] = useState(false);
+  const [winMessage, setwinMessage] = useState("");
+  const [flag, setflag] = useState(false);
+
+  let nullCount = 9;
+
+  function changeIcon(index) {
+    // console.log("Cell Cliked");
+    if (winMessage) {
+      console.log("You Win the Game");
+    }
+    if (array[index] === 'null') {
+      array[index] = isCross ? "cross" : "circle";
+      setisCross(!isCross)
+    } else {
+      alert("Already Filled !")
+    }
+    checkWinner()
+    checkForDraw()
+    console.log(flag);
+  }
+
+  function checkWinner() {
+    if (array[0] === array[1] && array[0] === array[2] && array[0] !== "null") {
+      setwinMessage(`${array[0]} Won the Game`);
+    } else if (array[3] === array[4] && array[3] === array[5] && array[3] !== "null") {
+      setwinMessage(`${array[3]} Won the Game`);
+    } else if (array[6] === array[7] && array[6] === array[8] && array[6] !== "null") {
+      setwinMessage(`${array[6]} Won the Game`);
+    } else if (array[0] === array[3] && array[0] === array[6] && array[0] !== "null") {
+      setwinMessage(`${array[0]} Won the Game`);
+    } else if (array[1] === array[4] && array[1] === array[7] && array[1] !== "null") {
+      setwinMessage(`${array[1]} Won the Game`);
+    } else if (array[2] === array[5] && array[2] === array[8] && array[2] !== "null") {
+      setwinMessage(`${array[2]} Won the Game`);
+    } else if (array[0] === array[4] && array[0] === array[8] && array[0] !== "null") {
+      setwinMessage(`${array[0]} Won the Game`);
+    } else if (array[2] === array[4] && array[2] === array[6] && array[2] !== "null") {
+      setwinMessage(`${array[2]} Won the Game`);
+    }
+  }
+
+  function reload() {
+    // console.log("Reload Game");
+    setisCross(false);
+    setwinMessage("");
+    array.fill('null', 0, 9);
+    setflag(false);
+  }
+
+  function checkForDraw() {
+    array.forEach(ele => {
+      if (ele !== 'null') {
+        nullCount--;
+        // console.log(nullCount);
+        if (nullCount === 0) {
+          setflag(true);
+        }
+      }
+    });
+  }
+
+  return (
+    <center>
+      {winMessage ? (
+        <div className='my-5'>
+          <h1 className='text-uppercase'>{winMessage}</h1>
+          <button type="button" className="btn btn-info" onClick={reload}>Reset</button>
+        </div>
+      ) : (
+        <div className='my-5'>
+          {
+            flag ? (
+              <h1 className='text-uppercase'>Match Draw</h1>
+            ) : (
+              <h1 className='text-uppercase'>Draw {isCross ? "Cross" : "Circle"}</h1>
+            )
+          }
+          {flag && <button type="button" className="btn btn-info" onClick={reload}>Reset</button>}
+        </div>
+      )}
+
+      <div className='grid'>
+        {
+          array.map((ele, index) => {
+            return (
+              <div key={index} onClick={() => { changeIcon(index) }}>
+                <div className="box">
+                  <Icon name={ele} />
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </center>
+
+  );
+}
+
+export default Home;
---
+++ src/Components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css';
+
+const Landing = () => {
+    return (
+        <center className="landing">
+            <div className='imgContainer'>
+                {/* <img src="https://i.ibb.co/M1hsqyz/AvGt.gif" alt="img" /> */}
+            </div>
+            <button className='startBtn'>Start Game</button>
+        </center>
+    )
+}
+
+export default Landing
